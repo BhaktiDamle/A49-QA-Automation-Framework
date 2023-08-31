37dc15bce72af79eae6833ec95394bb484b1c401
BhaktiDamle <bhakti.damle@testpro.io>
2023-08-31 14:59:38 -0700

    homework-22

---
+++ src/test/java/HomePageTests.java
@@ -0,0 +1,27 @@
+import Pages.LoginPage;
+import org.testng.annotations.Test;
+import Pages.HomePage;
+
+
+public class HomePageTests extends BaseTest{
+
+    @Test
+
+    public void contextClick(){
+
+        LoginPage loginPage= new LoginPage(driver);
+        HomePage homePage = new HomePage(driver);
+        loginPage.loginCorrectCred();
+        homePage.clickAllSongs();
+        homePage.contextClickFirstSong();
+    }
+
+public void hoverPlay(){
+
+    LoginPage loginPage= new LoginPage(driver);
+    HomePage homePage = new HomePage(driver);
+    loginPage.loginCorrectCred();
+    homePage.clickAllSongs();
+    homePage.hoverOverPlayButton();
+}
+}
