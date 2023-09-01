fcd5f35dd7585f15cb741b435c63a53855dc42fb
BhaktiDamle <bhakti.damle@testpro.io>
2023-09-01 14:46:28 -0700

    update homework-22

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,20 +1,12 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeSuite;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
-
-import java.time.Duration;
 
 public class BaseTest {
 
