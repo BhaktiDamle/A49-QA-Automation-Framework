6c2c4088ca0fee8593ff5f237b0f67275943de1d
BhaktiDamle <bhakti.damle@testpro.io>
2023-09-15 14:47:37 -0700

    homework-27

--- src/test/java/CucumberRunner.java
+++ src/test/java/CucumberRunner.java
@@ -5,9 +5,12 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 
+
 @CucumberOptions(
 
-        features = {"src/test/resources/features/Login.feature"}
+        features = {"src/test/resources/features/Login.feature"},publish = true
+
+
 )
 
 public class CucumberRunner extends AbstractTestNGCucumberTests {
