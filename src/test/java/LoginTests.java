18eb4145da831d03e5b40ddd41c79d6886b68c67
BhaktiDamle <bhakti.damle@testpro.io>
2023-09-22 11:35:19 -0700

    homework-29

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,32 +1,32 @@
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import Pages.LoginPage;
 
 public class LoginTests extends BaseTest {
 
-
-    @Test
-
-    public void loginCorrectCred() {
-        provideEmail("bhakti.damle@testpro.io");
-        providePassword("Mangodesk234!");
-        clickSubmit();
-        Assert.assertTrue(isAvatarDisplayed());
-    }
+//    @Test
+//
+//    public void loginCorrectCred(){
+//        provideEmail("bhakti.damle@testpro.io");
+//        providePassword("Mangodesk234!");
+//        clickSubmit();
+//        Assert.assertTrue(isAvatarDisplayed());
+  //  }
 @Test
-    public void loginIncorrectEmailPasswordCred(){
-        provideEmail("bhakti@testpro.io");
+   public void loginIncorrectEmailPasswordCred(){
+       provideEmail("bhakti@testpro.io");
         providePassword("Mangodesk");
-        clickSubmit();
+       clickSubmit();
         Assert.assertEquals(driver.getCurrentUrl()," https://qa.koel.app/");
     }
-@Test
-    public void loginValidEmailEmptyPassword(){
 
-        provideEmail("bhakti@testpro.io");
-        providePassword("");
-        clickSubmit();
-        Assert.assertEquals(driver.getCurrentUrl(), " https://qa.koel.app/");
-    }
+
+//@Test
+//    public void loginValidEmailEmptyPassword(){
+//
+//        provideEmail("bhakti@testpro.io");
+//        providePassword("");
+//        clickSubmit();
+//        Assert.assertEquals(driver.getCurrentUrl(), " https://qa.koel.app/");
+//    }
 
 }
